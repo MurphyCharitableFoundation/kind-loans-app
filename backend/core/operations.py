9c6728bda371d9e98b55da4dd3f48700be6af799
Ayo Onipe <ayosemail@gmail.com>
2024-12-18 15:23:31 -0700

    Fix operations platform accounts

--- backend/core/operations.py
+++ backend/core/operations.py
@@ -1,29 +1,43 @@
 from hordak.models import Account
 
 
-PLATFORM_CASH_ACCOUNT, _ = Account.objects.get_or_create(
-    name="platform_cash_account"
-)
-PLATFORM_BAD_DEBT, _ = Account.objects.get_or_create(name="platform_bad_debt")
-PLATFORM_ACCOUNT_PAYABLES, _ = Account.objects.get_or_create(
-    name="platform_account_payables"
-)
-PLATFORM_ACCOUNT_RECEIVABLES, _ = Account.objects.get_or_create(
-    name="platform_account_receivables"
-)
+def config_platform_accounts():
+    PLATFORM_CASH_ACCOUNT, _ = Account.objects.get_or_create(
+        name="platform_cash_account"
+    )
+    PLATFORM_BAD_DEBT, _ = Account.objects.get_or_create(
+        name="platform_bad_debt"
+    )
+    PLATFORM_ACCOUNT_PAYABLES, _ = Account.objects.get_or_create(
+        name="platform_account_payables"
+    )
+    PLATFORM_ACCOUNT_RECEIVABLES, _ = Account.objects.get_or_create(
+        name="platform_account_receivables"
+    )
+
+    return {
+        "bad_debt": PLATFORM_BAD_DEBT,
+        "cash": PLATFORM_CASH_ACCOUNT,
+        "payables": PLATFORM_ACCOUNT_PAYABLES,
+        "receivables": PLATFORM_ACCOUNT_RECEIVABLES,
+    }
 
 
 def lender_to_app(lender, amount):
-    return PLATFORM_ACCOUNT_PAYABLES.transfer_to(
-        to_account=PLATFORM_CASH_ACCOUNT,
+    accounts = config_platform_accounts()
+
+    return accounts["payables"].transfer_to(
+        to_account=accounts["cash"],
         amount=amount,
         description=f"User {lender} added funds to their account",
     )
 
 
 def app_to_borrower(borrower, amount):
-    return PLATFORM_CASH_ACCOUNT.transfer_to(
-        to_account=PLATFORM_ACCOUNT_RECEIVABLES,
+    accounts = config_platform_accounts()
+
+    return accounts["cash"].transfer_to(
+        to_account=accounts["receivables"],
         amount=amount,
         description=f"Disbursed funds to {borrower}",
     )
@@ -33,23 +47,27 @@ def borrower_to_app(borrower, amount, amount_outstanding):
     """
     Borrower repaid app total amount that they could.
     """
+    accounts = config_platform_accounts()
+
     if amount_outstanding:
-        PLATFORM_ACCOUNT_RECEIVABLES.transfer_to(
-            to_account=PLATFORM_BAD_DEBT,
+        accounts["receivables"].transfer_to(
+            to_account=accounts["bad_debt"],
             amount=amount_outstanding,
             description=f"Written off as bad debt; non-payment by {borrower}",
         )
 
-    return PLATFORM_ACCOUNT_RECEIVABLES.transfer_to(
-        to_account=PLATFORM_CASH_ACCOUNT,
+    return accounts["receivables"].transfer_to(
+        to_account=accounts["cash"],
         amount=amount,
         description=f"Borrower {borrower} repaid app",
     )
 
 
 def app_to_lender(lender, amount):
-    return PLATFORM_CASH_ACCOUNT.transfer_to(
-        to_account=PLATFORM_ACCOUNT_PAYABLES,
+    accounts = config_platform_accounts()
+
+    return accounts["cash"].transfer_to(
+        to_account=accounts["payables"],
         amount=-amount,
         description=f"Payout to lender: {lender}",
     )
