b264c733dca1b6a649c956ba5f96847b6ffbb0f8
Ken Ding <ken.ding@mail.com>
2025-02-08 01:00:09 +1100

    26 - fix styles issue

--- backend/app/envs/development.py
+++ backend/app/envs/development.py
@@ -4,7 +4,9 @@ Development settings for app project.
 from .base import *  # noqa
 
 DEBUG = True
-SECRET_KEY = "django-insecure-!0%pkeilx*h-*iz@!^lfs_7irdbcaajph70s--6ibi_2pcz9y9"
+SECRET_KEY = (
+    "django-insecure-!0%pkeilx*h-*iz@!^lfs_7irdbcaajph70s--6ibi_2pcz9y9"
+)
 
 BASE_FRONTEND_URL = "http://localhost:3000"
 
@@ -25,5 +27,9 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 PAYPAL_TEST = True
 
 # ALLAUTH redirect URLs
-EMAIL_CONFIRM_REDIRECT_BASE_URL = f"{BASE_FRONTEND_URL}/email/confirm/"
-PASSWORD_RESET_CONFIRM_REDIRECT_BASE_URL = f"{BASE_FRONTEND_URL}/password-reset/confirm/"
+EMAIL_CONFIRM_REDIRECT_BASE_URL = (
+    f"{BASE_FRONTEND_URL}/email/confirm/"
+)
+PASSWORD_RESET_CONFIRM_REDIRECT_BASE_URL = (
+    f"{BASE_FRONTEND_URL}/password-reset/confirm/"
+)
